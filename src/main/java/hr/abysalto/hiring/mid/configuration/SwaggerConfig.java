3fe72451fe6beba4fa899873de657c4bf380f4fc
Vladimir <vladimir.gorgieski@hotmail.com>
2026-03-22 21:17:20 +0100

    MID-010: Add swagger title

--- src/main/java/hr/abysalto/hiring/mid/configuration/SwaggerConfig.java
+++ src/main/java/hr/abysalto/hiring/mid/configuration/SwaggerConfig.java
@@ -1,10 +1,25 @@
 package hr.abysalto.hiring.mid.configuration;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(info = @Info(
+		title = "Abysalto MID API",
+		version = "1.0",
+		description = "User, Product & Cart Management"
+))
+@SecurityScheme(
+		name = "bearerAuth",
+		type = SecuritySchemeType.HTTP,
+		scheme = "bearer",
+		bearerFormat = "JWT"
+)
 public class SwaggerConfig {
 	@Bean
 	public GroupedOpenApi publicApi() {
