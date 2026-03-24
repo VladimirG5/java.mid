0b2f7875173729224814e2d65650153a60810e4f
Vladimir <vladimir.gorgieski@hotmail.com>
2026-03-25 00:20:55 +0100

    MID-015: Dockerized the application and added deployment support for kubernetes (minikube)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
-import Navbar from './components/Navbar'
+import Navbar from './components/NavBar'
 import ProtectedRoute from './components/ProtectedRoute'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
